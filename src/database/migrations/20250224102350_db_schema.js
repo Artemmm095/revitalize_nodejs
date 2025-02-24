87cfc4043a499e66429ee791424bc5e869ce5b09
Artem <artemmm095@gmail.com>
2025-02-24 16:08:05 +0200

    Changed migration file: added new tables: workouts, activities, metrics, activities_metrics, workouts_metrics

---
+++ src/database/migrations/20250224102350_db_schema.js
@@ -0,0 +1,83 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = (knex) => {
+  knex.schema.createTable('users', (table) => {
+    table.increments('user_id');
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    table.string('country').notNullable();
+    table.text('avatar');
+    table.timestamps(true, true);
+  });
+
+  knex.schema.createTable('workouts', (table) => {
+    table.increments('workout_id');
+    table.integer('user_id').unsigned().notNullable()
+      .references('user_id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.integer('activity_id').unsigned().notNullable()
+      .references('activity_id')
+      .inTable('activities')
+      .onDelete('CASCADE');
+    table.string('title');
+    table.text('commentary');
+    table.time('duration', 0).notNullable();
+    table.timestamps(true, true);
+  });
+
+  knex.schema.createTable('activities', (table) => {
+    table.increments('activity_id');
+    table.string('name').notNullable().unique();
+  });
+
+  knex.schema.createTable('metrics', (table) => {
+    table.increments('metric_id');
+    table.string('name').notNullable().unique();
+    table.string('units').notNullable();
+  });
+
+  knex.schema.createTable('activities_metrics', (table) => {
+    table.integer('activity_id').unsigned().notNullable()
+      .references('activity_id')
+      .inTable('activities')
+      .onDelete('CASCADE');
+    table.integer('metric_id').unsigned().notNullable()
+      .references('metric_id')
+      .inTable('metrics')
+      .onDelete('CASCADE');
+    table.primary(['activity_id', 'metric_id']);
+  });
+
+  knex.schema.createTable('workouts_metrics', (table) => {
+    table.integer('workout_id').unsigned().notNullable()
+      .references('workout_id')
+      .inTable('workouts')
+      .onDelete('CASCADE');
+    table.integer('metric_id').unsigned().notNullable()
+      .references('metric_id')
+      .inTable('metrics')
+      .onDelete('CASCADE');
+    table.decimal('metric_value', 10, 2).notNullable();
+    table.primary(['workout_id', 'metric_id']);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.down = (knex) => {
+  knex.schema.dropTable('users');
+  knex.schema.dropTable('workouts');
+  knex.schema.dropTable('activities');
+  knex.schema.dropTable('metrics');
+  knex.schema.dropTable('activities_metrics');
+  knex.schema.dropTable('workouts_metrics');
+};
